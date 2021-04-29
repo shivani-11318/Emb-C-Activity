e0caa229d36dd7e646977486f9711c67e1603349
shivani-11318 <shivanirajput500@gmail.com>
2021-04-29 17:02:00 +0530

    new commit

--- src/Activity3.c
+++ src/Activity3.c
@@ -9,10 +9,9 @@
 
 void initTimer(void){
 
-TCCR0A|=(1<<COM0A1)|(1<<WGM00)|(1<<WGM00);
-    // Insert code
-TCCR0B|=(1<<CS01)|(1<<CS00);
-DDRD|=(1<<PD6);
+TCCR1A|=(1<<COM1A1)|(1<<WGM10)|(1<<WGM11);
+TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10);
+DDRB|=(1<<PB1);
 
 
 }
@@ -27,29 +26,29 @@ if((output_adc>=0) && (output_adc<=209)){
         OCR1A = 205;    //20% duty cycle
         data_uart = 'A';
 
-        _delay_ms(200);
+        _delay_ms(2000);
     }
     else if((output_adc>=210) && (output_adc<=509)){
 
         OCR1A = 410;    //40% duty cycle
         data_uart = 'B';
-        _delay_ms(200);
+        _delay_ms(2000);
     }
     else if((output_adc>=510) && (output_adc<=709)){
 
         OCR1A = 717;    //70% duty cycle
         data_uart ='C';
-        _delay_ms(200);
+        _delay_ms(2000);
     }
     else if((output_adc>=710) && (output_adc<=1024)){
 
         OCR1A = 973;    //95% duty cycle
         data_uart = 'D';
-        _delay_ms(200);
+        _delay_ms(2000);
     }
     else{
         OCR1A = 0;  //0% output
-        data_uart = '0';
+        
     }
 return data_uart;
 }
